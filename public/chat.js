412952e3e5e5fedbd2754b9c3c6119cdc02e7d75
Vinay Bhat <vinayks.bhatt@gmail.com>
2020-02-05 12:03:56 +0530

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -1,7 +1,7 @@
 
 $(function(){
    	//make connection
-	var socket = io.connect('http://localhost:3000')
+	var socket = io.connect('https://super-chat-box.herokuapp.com/')
 
 	//buttons and inputs
 	var message = $("#message")
